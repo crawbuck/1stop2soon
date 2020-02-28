617ab5bb1469dc5a5b1009592b27e56cd00536c0
Michael Crawbuck <crawbuck@Michaels-MacBook-Pro-3.local>
2020-02-28 09:18:34 -0500

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header title="one stop" />
+
+    <Footer title="too soon." />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  body {
+    margin: 0;
+    background: #2C2C36;
+    color: #F3EDE0;
+    height: 100vh;
+    width: 100%;
+    overflow: hidden;
+  }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <h1>{{ title }}</h1>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
