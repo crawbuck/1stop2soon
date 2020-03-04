d482849a8e86e2d38fff8d01cd02d7f41a7c817c
Michael Crawbuck <crawbuck@Michaels-MBP-3.fios-router.home>
2020-03-03 19:56:54 -0500

    wip

--- src/App.vue
+++ src/App.vue
@@ -20,31 +20,4 @@ export default {
 
 <style lang="scss">
   @import './styles/style.scss';
-  @font-face {
-    font-family: 'untitled';
-    src: url('assets/untitled-sans-medium.eot'); /* IE9 Compat Modes */
-    src: url('assets/untitled-sans-medium.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-         url('assets/untitled-sans-medium.woff2') format('woff2'), /* Super Modern Browsers */
-         url('assets/untitled-sans-medium.woff') format('woff'), /* Pretty Modern Browsers */
-         url('assets/untitled-sans-medium.ttf')  format('truetype'); /* Safari, Android, iOS */
-  }
-  body {
-    margin: 0;
-    background: #2C2C36;
-    color: #F3EDE0;
-    height: 100vh;
-    width: 100%;
-    overflow: hidden;
-    font-family: 'untitled', helvetica;
-    text-transform: uppercase;
-    letter-spacing: .2rem;
-    font-size: 2rem;
-    position: relative;
-  }
-  ul {
-    padding: 0;
-    margin: 0;
-    display: none;
-    list-style: none;
-  }
 </style>
